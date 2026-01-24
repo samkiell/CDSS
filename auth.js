e4930e15ce263a9c2acc2b9031c2b80746d5755a
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-24 17:52:04 +0100

    feat: update authentication middleware by adding new file and removing deprecated middleware

--- auth.js
+++ auth.js
@@ -1,6 +1,18 @@
+import { User } from '@/models';
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Credentials()],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        // Here you would normally fetch the user from your database
+      },
+    }),
+  ],
 });
