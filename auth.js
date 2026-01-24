4c742fa3e644ad046ee1f840a5c2b03ccfb64a89
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-24 17:41:41 +0100

    feat: configure NextAuth to use Credentials provider for authentication

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Credentials()],
 });
