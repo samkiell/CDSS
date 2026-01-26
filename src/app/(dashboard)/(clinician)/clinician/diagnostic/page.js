105c789c8d09124a0d35190ca834e3e0229b8f1b
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-26 04:51:44 +0100

    feat: add patient data to DiagnosticModePage for improved display

--- src/app/(dashboard)/(clinician)/clinician/diagnostic/page.js
+++ src/app/(dashboard)/(clinician)/clinician/diagnostic/page.js
@@ -7,38 +7,38 @@ export default function DiagnosticModePage() {
   const [searchQuery, setSearchQuery] = useState('');
 
   const patients = [
-    // {
-    //   id: 1,
-    //   name: 'Bola Ahmed Tinubu',
-    //   gender: 'Male',
-    //   time: '8:00am',
-    //   date: '12-11-2025',
-    //   status: 'active',
-    // },
-    // {
-    //   id: 2,
-    //   name: 'Tomisi Faith John',
-    //   gender: 'Female',
-    //   time: '8:30am',
-    //   date: '12-11-2025',
-    //   status: 'active',
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Henry Ahmed Garet',
-    //   gender: 'Male',
-    //   time: '10:00am',
-    //   date: '12-11-2025',
-    //   status: 'pending',
-    // },
-    // {
-    //   id: 4,
-    //   name: 'Bola Saed Dushak',
-    //   gender: 'Male',
-    //   time: '10:30am',
-    //   date: '12-11-2025',
-    //   status: 'urgent',
-    // },
+    {
+      id: 1,
+      name: 'Bola Ahmed Tinubu',
+      gender: 'Male',
+      time: '8:00am',
+      date: '12-11-2025',
+      status: 'active',
+    },
+    {
+      id: 2,
+      name: 'Tomisi Faith John',
+      gender: 'Female',
+      time: '8:30am',
+      date: '12-11-2025',
+      status: 'active',
+    },
+    {
+      id: 3,
+      name: 'Henry Ahmed Garet',
+      gender: 'Male',
+      time: '10:00am',
+      date: '12-11-2025',
+      status: 'pending',
+    },
+    {
+      id: 4,
+      name: 'Bola Saed Dushak',
+      gender: 'Male',
+      time: '10:30am',
+      date: '12-11-2025',
+      status: 'urgent',
+    },
   ];
 
   const filteredPatients = patients.filter((patient) =>
